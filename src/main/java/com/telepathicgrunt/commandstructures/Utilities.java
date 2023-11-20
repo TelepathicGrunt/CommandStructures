bcb9fae82c59df6032bc4700a9b912699c644361
TelepathicGrunt <40846040+TelepathicGrunt@users.noreply.github.com>
2023-11-20 18:46:54 -0500

    updated to 1.20.2

--- src/main/java/com/telepathicgrunt/commandstructures/Utilities.java
+++ src/main/java/com/telepathicgrunt/commandstructures/Utilities.java
@@ -2,7 +2,6 @@ package com.telepathicgrunt.commandstructures;
 
 import com.telepathicgrunt.commandstructures.mixin.ChunkMapAccessor;
 import net.minecraft.network.protocol.game.ClientboundLevelChunkWithLightPacket;
-import net.minecraft.server.level.ChunkHolder;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.world.level.ChunkPos;
 import net.minecraft.world.level.chunk.ChunkAccess;
@@ -12,7 +11,7 @@ public final class Utilities {
     private Utilities() {}
 
     public static void refreshChunksOnClients(ServerLevel level) {
-        int viewDistance = ((ChunkMapAccessor)level.getChunkSource().chunkMap).getViewDistance();
+        int viewDistance = ((ChunkMapAccessor)level.getChunkSource().chunkMap).getServerViewDistance();
         level.players().forEach(player -> {
             for(int x = -viewDistance; x <= viewDistance; x++) {
                 for(int z = -viewDistance; z <= viewDistance; z++) {
@@ -20,8 +19,6 @@ public final class Utilities {
                         ChunkAccess chunkAccess = level.getChunk(new ChunkPos(player.chunkPosition().x + x, player.chunkPosition().z + z).getWorldPosition());
                         if(chunkAccess instanceof LevelChunk levelChunk) {
                             ClientboundLevelChunkWithLightPacket lightPacket = new ClientboundLevelChunkWithLightPacket(levelChunk, level.getLightEngine(), null, null);
-                            player.untrackChunk(levelChunk.getPos());
-                            player.trackChunk(levelChunk.getPos(), lightPacket);
                         }
                     }
                 }
